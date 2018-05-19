3631d559c6c52b0e6297565dcf2a2c6a945f6a98
fengg5241 <fengg_5241@163.com>
2018-05-19 23:34:16 +0800

    create spring boot project

---
+++ src/main/java/com/sp/friend/SpFriendApplication.java
@@ -0,0 +1,12 @@
+package com.sp.friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpFriendApplication.class, args);
+	}
+}
